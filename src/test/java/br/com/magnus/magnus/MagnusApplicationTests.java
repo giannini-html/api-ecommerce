9a5be18b4f43f8999dbad95c1100b7a722d67a9d
Gustavo Giannini <gustavo-giannini@hotmail.com>
2023-02-17 18:03:25 -0300

    Criado projeto Rest

---
+++ src/test/java/br/com/magnus/magnus/MagnusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magnus.magnus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
